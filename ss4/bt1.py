ac7a2b05ddfd95f641e961e64f898154445e5411
dungbede16827-bit <dungbede16827@gmail.com>
2026-05-20 22:10:03 +0700

    Add files via upload

---
+++ ss4/bt1.py
@@ -0,0 +1,12 @@
+tong_tien = int(input("Nhập tổng tiền hóa đơn ban đầu: "))
+
+if tong_tien >= 500000:
+    giam_gia = tong_tien * 10 // 100
+else:
+    giam_gia = 0
+
+thanh_toan = tong_tien - giam_gia
+
+print("HÓA ĐƠN THANH TOÁN RIKKEI STORE")
+print("Số tiền được giảm giá:", giam_gia, "VND")
+print("Tổng tiền khách phải trả:", thanh_toan, "VND")
